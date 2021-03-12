7590c02976c8fb84728e94833912e937318cbd18
naim380 <naim.ebrahim@le-campus-numerique.fr>
2021-03-12 16:10:46 +0100

    vuejs

---
+++ script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello Vue !'
+    }
+})
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        title: 'where are you ?!'
+    }
+})
+//app.message = 'I changed data!';
