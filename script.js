42c2b72b4a281ba13da2113957182da1e9440d13
naim380 <naim.ebrahim@le-campus-numerique.fr>
2021-03-16 15:09:12 +0100

    maj

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var app = new Vue({
     data: {
         title: 'Hello Vue !',
         title2: 'where are you ?!',
+        title3: "",
         show : false,
         active: true,
         show2: false,
+        active2: true,
     }
 })
 
